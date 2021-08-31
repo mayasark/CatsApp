72f68dc4c2594618e0617a5081a2d68832cbe990
mayasark <57621881+mayasark@users.noreply.github.com>
2021-09-01 00:50:09 +0300

    fix useEffect

--- src/components/CatRender.js
+++ src/components/CatRender.js
@@ -1,56 +1,53 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
-import {service} from "../service/Service";
-import {Button, Image}  from "./Wrapper";
-import Spinner from "./Spinner"
+import { service } from "../service/Service";
+import { Button, Image } from "./Wrapper";
+import Spinner from "./Spinner";
 
 const CatRender = ({ enabled, autoRefresh }) => {
+  const [cat, setCat] = useState({});
+  const [isSendRequest, setIsSendRequest] = useState(true);
 
-    const [cat, setCat] = useState({});
-    const [isSendRequest, setIsSendRequest] = useState(true);
-  
-    const getCat = () => {
-      service.getCat()
-        .then((res) => {
-          res?.length && setCat(res[0]);
-          setIsSendRequest(false);
-        });
-    
+  const getCat = () => {
+    service.getCat().then((res) => {
+      res?.length && setCat(res[0]);
+      setIsSendRequest(false);
+    });
+  };
 
+  useEffect(() => {
+    if (isSendRequest) {
+      getCat();
     }
-    useEffect(() => {
-      if(isSendRequest){ 
-        getCat();
-      }
-    },
-    [isSendRequest]);
+  }, [isSendRequest]);
 
-    useEffect(() => {
-      let interval = null;
-      if(autoRefresh && enabled){
-        interval = setInterval(() => {
-          getCat();
-        }, 5000);
-      }
-      return () => {
-        if(interval) 
-          clearInterval(interval);
-      }
-    }, 
-    [autoRefresh, enabled]
+  useEffect(() => {
+    let interval = null;
+    if (autoRefresh && enabled) {
+      interval = setInterval(() => {
+        setIsSendRequest(true);
+      }, 5000);
+    }
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, [autoRefresh, enabled]);
 
-    );
-
-    return (
-      <div>
-        <Button disabled={isSendRequest || !enabled} onClick={() => setIsSendRequest(true)}> 
-          Get cat
-        </Button>
-        { (!isSendRequest && cat?.url ) ? <Image alt="cat" src={cat?.url}/> : <Spinner />}
-      </div>
-    )
-
-}
+  return (
+    <div>
+      <Button
+        disabled={isSendRequest || !enabled}
+        onClick={() => setIsSendRequest(true)}
+      >
+        Get cat
+      </Button>
+      {!isSendRequest && cat?.url ? (
+        <Image alt="cat" src={cat?.url} />
+      ) : (
+        <Spinner />
+      )}
+    </div>
+  );
+};
 
 export default CatRender;
-
